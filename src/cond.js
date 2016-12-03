88430922831372c3892b80cedd641749515a719d
Kai Curry <kai@cape.io>
2016-12-03 15:50:53 -0600

    cleanup

--- src/cond.js
+++ src/cond.js
@@ -1,6 +1,9 @@
-import { concat, cond, identity, stubTrue } from 'lodash'
+import { concat, cond, curry, identity, rearg, stubTrue } from 'lodash'
+
+export function branch(bool, trueVal, falseVal) { return bool ? trueVal : falseVal }
+export const fpBranch = curry(rearg(branch, [1, 2, 0]), 3)
 
 export function condId(...conditions) {
-  const rules = concat(conditions, [ [ stubTrue, identity ] ])
+  const rules = concat(conditions, [[stubTrue, identity]])
   return cond(rules)
 }

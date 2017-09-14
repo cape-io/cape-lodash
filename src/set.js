45ba9eb5410142ae5efe0fe1dacd46fb003bb450
Kai Curry <kai@cape.io>
2017-09-13 23:39:20 -0400

    v1.13.0 set should work with path strings

--- src/set.js
+++ src/set.js
@@ -1,20 +1,22 @@
-import { curry, get, omit, rearg } from 'lodash'
+import { curry, get, omit, rearg, toPath } from 'lodash'
 import { has } from 'lodash/fp'
 import { condId } from './cond'
 import { doProp } from './transform'
 
-export function set(state, key, value) { return { ...state, [key]: value } }
-// setKey(key, state, value)
+export function setSimple(state, key, value) { return { ...state, [key]: value } }
+export const set = curry((state, path, value) => {
+  const [key, ...rest] = toPath(path)
+  if (!rest.length) return setSimple(state, key, value)
+  return setSimple(state, key, set(get(state, key, {}), rest, value))
+})
+// setKey(path, state, value)
 export const setKey = curry(rearg(set, [1, 0, 2]), 3)
+export const setIn = setKey
 // setKeyVal(key, value, state)
 export const setKeyVal = curry(rearg(set, [2, 0, 1]), 3)
 // setVal(value, state, key)
 export const setVal = curry(rearg(set, [1, 2, 0]), 3)
-// setIn(pathArray, state, value)
-export const setIn = curry(([key, ...rest], state, value) => {
-  if (!rest.length) return set(state, key, value)
-  return set(state, key, setIn(rest, get(state, key, {}), value))
-})
+
 export const setField = curry((path, transformer) => item => set(item, path, transformer(item)))
 export const setFieldHas = curry((path, transformer) => condId([
   has(path), setField(path, transformer),
@@ -26,6 +28,6 @@ export const setWith = curry((fieldId, withId, transformer) =>
   setField(fieldId, doProp(transformer, withId))
 )
 export function delAt(path, state) {
-  const omitKey = path.pop()
-  return setIn(path, state, omit(get(state, path), omitKey))
+  const omitKey = toPath(path).pop()
+  return setKey(path, state, omit(get(state, path), omitKey))
 }
--- test/set.spec.js
+++ test/set.spec.js
@@ -2,16 +2,42 @@ import test from 'tape'
 import { has, isFunction, method } from 'lodash'
 import { multiply } from 'lodash/fp'
 import {
-  replaceField, setField, setFieldHas, setKey, setKeyVal, setIn, setVal, transformProp,
+  replaceField, set, setField, setFieldHas, setKey, setKeyVal, setSimple, setVal, transformProp,
 } from '../src'
 import { collection } from './mock'
 
+test('setSimple', (t) => {
+  const obj = { foo: 'cat' }
+  const obj2 = setSimple(obj, 'bar', 'dog')
+  t.false(obj2 === obj)
+  t.equal(obj2.foo, 'cat')
+  t.equal(obj2.bar, 'dog')
+  t.end()
+})
+test('set', (t) => {
+  const updateTitle = set(collection)
+  const res1 = updateTitle(['a1', 'title'], 'apples')
+  t.false(collection === res1)
+  t.false(collection.a1 === res1.a1, 'a1')
+  // console.log(res1)
+  t.equal(res1.a1.title, 'apples', 'title')
+  t.equal(collection.a1.creator, res1.a1.creator, 'a1 creator')
+  t.equal(collection.a2, res1.a2, 'a2')
+  t.equal(collection.a3, res1.a3, 'a3')
+  const res2 = set(collection, ['a3', 'creator', 'anon', 'name'], 'drone')
+  t.equal(collection.a1.creator.anon, res2.a1.creator.anon)
+  t.equal(res2.a3.creator.anon.name, 'drone')
+  const res3 = set({}, 'foo.bar.song', 'valueThingy')
+  t.equal(res3.foo.bar.song, 'valueThingy', 'set in empty obj')
+  t.end()
+})
 test('setKey', (t) => {
   const setFoo = setKey('foo')
   t.ok(isFunction(setFoo), 'isFunction')
   const obj = { foo: 'cat' }
   const obj2 = setFoo(obj, 'dog')
   t.false(obj === obj2, 'not the same obj')
+  // console.log(obj2)
   t.equal(obj2.foo, 'dog')
   const obj3 = setKey('bar', obj2, 'ice')
   t.false(obj3 === obj2)
@@ -22,22 +48,6 @@ test('setKeyVal', (t) => {
   const func = setKeyVal('kai', 'isNerd')
   t.ok(isFunction(func))
   t.equal(func({}).kai, 'isNerd')
-  t.end()
-})
-test('setIn', (t) => {
-  const updateTitle = setIn(['a1', 'title'])
-  const res1 = updateTitle(collection, 'apples')
-  t.false(collection === res1)
-  t.false(collection.a1 === res1.a1, 'a1')
-  t.equal(res1.a1.title, 'apples', 'title')
-  t.equal(collection.a1.creator, res1.a1.creator, 'a1 creator')
-  t.equal(collection.a2, res1.a2, 'a2')
-  t.equal(collection.a3, res1.a3, 'a3')
-  const res2 = setIn(['a3', 'creator', 'anon', 'name'], collection, 'drone')
-  t.equal(collection.a1.creator.anon, res2.a1.creator.anon)
-  t.equal(res2.a3.creator.anon.name, 'drone')
-  const res3 = setIn(['foo', 'bar', 'song'], {}, 'valueThingy')
-  t.equal(res3.foo.bar.song, 'valueThingy', 'set in empty obj')
   t.end()
 })
 test('setField', (t) => {
